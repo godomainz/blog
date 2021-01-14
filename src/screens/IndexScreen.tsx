b24a8b8643a5cc066f96a6f10aea5f3233e6d68d
Akila Mendis <akila@oneit.com.au>
2021-01-14 10:43:06 +0530

    125 Initial Setup

---
+++ src/screens/IndexScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const IndexScreen = () => {
+
+    return (
+        <View>
+            <Text>Hello from IndexScreen</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
