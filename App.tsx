b6a7a880d7dca1f2863135e829060d3471bd4e06
Akila Mendis <akila@oneit.com.au>
2021-01-14 10:54:36 +0530

    126 Wrapping the Navigator

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
@@ -13,5 +14,7 @@ const navigator = createStackNavigator(
     }
   }
 );
-
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+export default () => {
+  return <App />
+}
