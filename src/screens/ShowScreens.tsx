46273f17acb30be96d5307c173cae5decc8a6e77
Akila Mendis <akila@oneit.com.au>
2021-01-15 20:30:03 +0530

    141 Retrieving Single Posts

--- src/screens/ShowScreens.tsx
+++ src/screens/ShowScreens.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import  { Context , BlogContextType } from '../context/BlogContext';
+import { BlogPost } from '../context/BlogPost';
 
-const ShowScreen = () => {
+const ShowScreen = ( { navigation }:any ) => {
+
+    const { state }  = useContext(Context) as BlogContextType;
+    const blogPost: BlogPost | undefined = state.find((blogPost)=> blogPost.id === navigation.getParam('id'));
 
     return (
         <View>
-            <Text>Hello from ShowScreen</Text>
+            <Text>{blogPost?.title}</Text>
         </View>
     );
 
