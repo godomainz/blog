e5a956c9c1640488c2748b9969d3498e8fc29c58
Akila Mendis <akila@oneit.com.au>
2021-01-16 09:07:20 +0530

    142 Adding a Creation Screen

---
+++ src/screens/CreateScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const CreateScreen = () => {
+
+    return (
+        <View>
+            <Text>Hello from CreateScreen</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
